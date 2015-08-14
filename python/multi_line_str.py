114f997a1b7b809899f9f0152cec9582942fe710
jnchen <caojingchen@live.com>
2015-08-14 13:46:56 +0800

    Add the example of multi_line_str and raw_str

---
+++ python/multi_line_str.py
@@ -0,0 +1,4 @@
+#/usr/bin/python
+str = '''"To be,or not to be",that is the question.
+Whether it's nobler in the mind to suffer.'''
+print(str)
---
+++ python/raw_str.py
@@ -0,0 +1,3 @@
+#/usr/bin/python
+str = r'\(~_~)/ \(~_~)/'
+print(str)
